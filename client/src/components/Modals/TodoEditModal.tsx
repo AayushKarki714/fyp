c45d0ab1ee5c4d45b36157ea0d93728881326508
Aayush <karkiayush201@gmail.com>
2023-01-31 13:13:02 +0545

    intgrate update todo title api in the front-end and migrate the code to TodoEdit modal

--- client/src/components/Modals/TodoEditModal.tsx
+++ client/src/components/Modals/TodoEditModal.tsx
@@ -1,7 +1,177 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import Modal from "../Modals/Modal";
+import Overlay from "../Modals/Overlay";
+import { useDrag } from "react-dnd/dist/hooks";
+import { ItemTypes } from "../../utils/ItemTypes";
+import { motion } from "framer-motion";
+import { addMonths, formatDistance } from "date-fns";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaceSmileIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { CalendarDaysIcon } from "@heroicons/react/24/outline";
+import { useMutation, useQueryClient } from "react-query";
+import axios from "../../api/axios";
+import { toast } from "react-toastify";
 
-function TodoEditModal() {
-  return <div>TodoEditModal</div>;
+interface Props {
+  title: string;
+  todo: any;
+}
+
+interface ITitlePayload {
+  text: string;
+}
+
+function TodoEditModal({ todo, title }: Props) {
+  const queryClient = useQueryClient();
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const {
+    createdAt,
+    completionDate,
+    description,
+    id: todoId,
+    todoCardId,
+  } = todo;
+  const [startDate, setStartDate] = useState<Date | null>(new Date(createdAt));
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [editTitleMode, setEditTitleMode] = useState<boolean>(false);
+  const [editTodoTitle, setEditTodoTitle] = useState<string>(title);
+  const [todoDescription, setTodoDescription] = useState<string>(
+    description || ""
+  );
+  const [editDescriptionMode, setEditDescriptionMode] = useState<boolean>(
+    !description
+  );
+
+  const updateTitleMutation = useMutation(
+    async (payload: ITitlePayload) => {
+      const res = await axios.patch(
+        `/todo/${todoCardId}/${todoId}/update-todo-title`,
+        payload
+      );
+      return res.data;
+    },
+    {
+      onError: (error) => {
+        console.log("error", error);
+      },
+      onSuccess: (data) => {
+        setEditTitleMode(false);
+        queryClient.invalidateQueries(["todo-query", todoCardId]);
+        console.log("data", data);
+      },
+    }
+  );
+
+  const handleTodoTitleUpdate = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!editTodoTitle)
+      return toast("Edited Todo title can't be Empty!!", {
+        position: "top-center",
+      });
+    updateTitleMutation.mutate({ text: editTodoTitle });
+  };
+
+  const formatTime = formatDistance(new Date(createdAt), new Date(), {
+    addSuffix: true,
+  });
+
+  useOnClickOutside(descriptionRef, () => {
+    if (description) {
+      setTodoDescription(description);
+      setEditDescriptionMode(false);
+    }
+  });
+
+  return (
+    <div className="w-[550px] h-[400px]">
+      <div className="flex flex-col gap-2 mb-4">
+        {editTitleMode ? (
+          <form onSubmit={handleTodoTitleUpdate}>
+            <input
+              id="edit-todo-title"
+              value={editTodoTitle}
+              onChange={(event) => setEditTodoTitle(event.target.value)}
+              className="bg-transparent border-none outline-none text-white text-2xl w-full "
+            />
+          </form>
+        ) : (
+          <h2 className="text-2xl" onDoubleClick={() => setEditTitleMode(true)}>
+            {title}
+          </h2>
+        )}
+      </div>
+      <h3 className="text-custom-light-green mb-4">Created {formatTime}</h3>
+      <div className="flex flex-col  mb-4 ">
+        <label
+          htmlFor="todo-description"
+          className="hover:text-custom-light-green"
+        >
+          Description:{" "}
+        </label>
+        {editDescriptionMode ? (
+          <textarea
+            ref={descriptionRef}
+            autoFocus
+            id="todo-description "
+            value={todoDescription}
+            placeholder="Enter a detailed description of the todo..."
+            className="w-full mt-2 resize-none h-24 rounded-md bg-custom-light-dark px-3 py-2 text-base custom-scrollbar text-gray-300 shadow"
+            onChange={(event) => setTodoDescription(event.target.value)}
+          />
+        ) : (
+          <div
+            className="mt-1 text-gray-400"
+            onClick={() => setEditDescriptionMode(true)}
+          >
+            <p>{todoDescription}</p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="date-picker"
+          className="flex gap-2 items-center group hover:text-custom-light-green"
+        >
+          Date Picker
+          <span>
+            <CalendarDaysIcon className="h-5 w-5 group-hover:text-custom-light-green" />
+          </span>
+        </label>
+        <DatePicker
+          className="bg-custom-light-dark px-4 py-2  rounded-md"
+          id="date-picker"
+          selected={endDate}
+          startDate={startDate}
+          placeholderText="Pick a Completion Date:"
+          minDate={new Date(createdAt)}
+          maxDate={addMonths(new Date(createdAt), 1)}
+          endDate={endDate}
+          selectsEnd
+          onChange={(endDate: any) => {
+            setEndDate(endDate);
+          }}
+        />
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="Enter a comment in todo" />
+          <div className="flex gap-2 items-center">
+            <button>
+              <FaceSmileIcon className="h-5 w-5 text-gray-400" />
+            </button>
+            <motion.button
+              whileTap={{ scale: 0.94 }}
+              className="flex items-center justify-center rounded-lg cursor-pointer"
+            >
+              <PaperAirplaneIcon className="h-5 text-gray-400 " />
+            </motion.button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default TodoEditModal;
--- client/src/components/Todo/Todo.tsx
+++ client/src/components/Todo/Todo.tsx
@@ -10,6 +10,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { FaceSmileIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { CalendarDaysIcon } from "@heroicons/react/24/outline";
+import TodoEditModal from "../Modals/TodoEditModal";
 
 interface TodoProps {
   title: string;
@@ -18,21 +19,11 @@ interface TodoProps {
 }
 
 const Todo: React.FC<TodoProps> = ({ title, todo, todoContainerId }) => {
-  const descriptionRef = useRef<HTMLTextAreaElement>(null);
-  const { createdAt, completionDate, description } = todo;
-  const [startDate, setStartDate] = useState<Date | null>(new Date(createdAt));
-  const [endDate, setEndDate] = useState<Date | null>(null);
-  const [editTitleMode, setEditTitleMode] = useState<boolean>(false);
-  const [editTodoTitle, setEditTodoTitle] = useState<string>(title);
-  const [todoDescription, setTodoDescription] = useState<string>(
-    description || ""
-  );
-  const [editDescriptionMode, setEditDescriptionMode] = useState<boolean>(
-    !description
-  );
-
   const [isOpen, setIsOpen] = useState(false);
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
   const [{ isDragging }, drag] = useDrag(() => ({
     type: `${ItemTypes.Todo}--${todoContainerId}`,
     item: { ...todo },
@@ -41,119 +32,11 @@ const Todo: React.FC<TodoProps> = ({ title, todo, todoContainerId }) => {
     }),
   }));
 
-  const closeModal = () => {
-    setEditTitleMode(false);
-    setIsOpen(false);
-  };
-
-  const handleTodoTitleUpdate = () => {};
-
-  const formatTime = formatDistance(new Date(createdAt), new Date(), {
-    addSuffix: true,
-  });
-
-  useOnClickOutside(descriptionRef, () => {
-    if (description) {
-      setTodoDescription(description);
-      setEditDescriptionMode(false);
-    }
-  });
-
   return (
     <>
       <Overlay isOpen={isOpen} onClick={closeModal}>
         <Modal onClick={closeModal}>
-          <div className="w-[550px] h-[400px]">
-            <div className="flex flex-col gap-2 mb-4">
-              {editTitleMode ? (
-                <form onSubmit={handleTodoTitleUpdate}>
-                  <input
-                    id="edit-todo-title"
-                    value={editTodoTitle}
-                    onChange={(event) => setEditTodoTitle(event.target.value)}
-                    className="bg-transparent border-none outline-none text-white text-2xl w-ful"
-                  />
-                </form>
-              ) : (
-                <h2
-                  className="text-2xl"
-                  onDoubleClick={() => setEditTitleMode(true)}
-                >
-                  {title}
-                </h2>
-              )}
-            </div>
-            <h3 className="text-custom-light-green mb-4">
-              Created {formatTime}
-            </h3>
-            <div className="flex flex-col  mb-4 ">
-              <label
-                htmlFor="todo-description"
-                className="hover:text-custom-light-green"
-              >
-                Description:{" "}
-              </label>
-              {editDescriptionMode ? (
-                <textarea
-                  ref={descriptionRef}
-                  autoFocus
-                  id="todo-description "
-                  value={todoDescription}
-                  placeholder="Enter a detailed description of the todo..."
-                  className="w-full mt-2 resize-none h-24 rounded-md bg-custom-light-dark px-3 py-2 text-base custom-scrollbar text-gray-300 shadow"
-                  onChange={(event) => setTodoDescription(event.target.value)}
-                />
-              ) : (
-                <div
-                  className="mt-1 text-gray-400"
-                  onClick={() => setEditDescriptionMode(true)}
-                >
-                  <p>{todoDescription}</p>
-                </div>
-              )}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="date-picker"
-                className="flex gap-2 items-center group hover:text-custom-light-green"
-              >
-                Date Picker
-                <span>
-                  <CalendarDaysIcon className="h-5 w-5 group-hover:text-custom-light-green" />
-                </span>
-              </label>
-              <DatePicker
-                className="bg-custom-light-dark px-4 py-2  rounded-md"
-                id="date-picker"
-                selected={endDate}
-                startDate={startDate}
-                placeholderText="Pick a Completion Date:"
-                minDate={new Date(createdAt)}
-                maxDate={addMonths(new Date(createdAt), 1)}
-                endDate={endDate}
-                selectsEnd
-                onChange={(endDate: any) => {
-                  setEndDate(endDate);
-                }}
-              />
-            </div>
-            <div>
-              <form>
-                <input type="text" placeholder="Enter a comment in todo" />
-                <div className="flex gap-2 items-center">
-                  <button>
-                    <FaceSmileIcon className="h-5 w-5 text-gray-400" />
-                  </button>
-                  <motion.button
-                    whileTap={{ scale: 0.94 }}
-                    className="flex items-center justify-center rounded-lg cursor-pointer"
-                  >
-                    <PaperAirplaneIcon className="h-5 text-gray-400 " />
-                  </motion.button>
-                </div>
-              </form>
-            </div>
-          </div>
+          <TodoEditModal title={title} todo={todo} />
         </Modal>
       </Overlay>
       <motion.div>
